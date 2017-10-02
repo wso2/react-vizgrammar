bf07ba15133336948abf7606eae5c7dbcb6e7266
Charuka Karunanayake <charukakarunanayake@gmail.com>
2017-10-02 14:53:00 +0530

    pie chart implementation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import LineChart from './Samples/LineChartsSamples';
 import BarChart from './Samples/BarChartConfigSample';
 import AreaChart from './Samples/AreaChartConfig';
 import ScatterPlot from './Samples/ScatterPlotConfigSample';
+import PieChart from './Samples/PieChartsConfigSample';
+import Test from './Samples/Test.js';
 
 import {
     BrowserRouter as Router,
@@ -16,10 +18,11 @@ ReactDOM.render(
 <Router>
     <div>
         <Route exact path="/" component={App}/>
-        {/* <Route exact path="/test" component={Test}/> */}
+        <Route exact path="/test" component={Test}/>
         <Route exact path="/line-charts" component={LineChart}/>
         <Route exact path="/bar-charts" component={BarChart}/>
         <Route exact path="/area-charts/" component={AreaChart}/>
         <Route exact path='/scatter-charts' component={ScatterPlot}/>
+        <Route exact path='/pie-charts' component={PieChart}/>
     </div>
 </Router>,document.getElementById('root'));
