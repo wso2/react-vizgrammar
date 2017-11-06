9d564ef49722f5c24fc94e8995562ee183f316b9
Charuka Karunanayake <charukakarunanayake@gmail.com>
2017-11-06 18:02:36 +0530

    fixing eslint issues in BasicChart.jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,59 @@
 module.exports = {
-    "parser": 'babel-eslint',
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+        }
+    },
     "env": {
         "browser": true,
         "commonjs": true,
-        "es6": true,
-        "node": true
+        "es6": true
     },
-    "extends": ["eslint:recommended", "plugin:react/recommended"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": ["react"],
+    "extends": "airbnb",
     "rules": {
-
-        "linebreak-style": ["warn", "unix"],
-        "quotes": ["warn", "single"],
-        "semi": ["warn", "always"],
-        "no-console": ["warn", {"allow": ["info", "error"]}]
-    }
-
-
+        "max-len": ["error", 120],
+        "require-jsdoc": ["warn", {
+            "require": {
+                "FunctionDeclaration": true,
+                "MethodDefinition": true,
+                "ClassDeclaration": true
+            }
+        }],
+        "valid-jsdoc": ["warn", {
+            "requireReturn": false
+        }],
+        "indent": ["error", 4, {"SwitchCase": 1}],
+        "import/no-extraneous-dependencies": ["off", {
+            "devDependencies": false,
+            "optionalDependencies": false,
+            "peerDependencies": false
+        }],
+        "import/no-unresolved": ["off"],
+        "import/extensions": ["off"],
+        "import/no-named-as-default": ["off"],
+        "import/no-named-as-default-member": ["off"],
+        "no-underscore-dangle": ["error", {
+            "allowAfterThis": true
+        }],
+        "no-param-reassign": ["off"],
+        "no-restricted-syntax": ["off"],
+        "no-plusplus": ["off"],
+        "func-names": ["off"],
+        "class-methods-use-this": ["off"],
+        "arrow-body-style": "off",
+        "prefer-template": "off",
+        "jsx-a11y/no-static-element-interactions": "off",
+        "jsx-a11y/no-noninteractive-element-interactions": "off",
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": ["error", 4],
+        "react/prefer-stateless-function": ["off"],
+        "no-mixed-operators": ["error", {"allowSamePrecedence": true}],
+        "jsx-quotes": ["off"],
+        "no-else-return": "off",
+    },
+    "plugins": [
+        "react"
+    ]
 };
