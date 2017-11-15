db94737e0d140e3ae137402391b5152d878d21b1
Charuka Karunanayake <charukakarunanayake@gmail.com>
2017-11-15 13:33:33 +0530

    time data handling changed

--- src/components/BasicChart.jsx
+++ src/components/BasicChart.jsx
@@ -40,6 +40,7 @@ import { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import ReactDOMServer from 'react-dom/server';
 import { getDefaultColorScale } from './helper';
+// import log from 'log';
 
 /**
  * React component required to render Bar, Line and Area Charts.
@@ -166,15 +167,8 @@ export default class BasicCharts extends React.Component {
 
                 dataSets[dataSetName] = dataSets[dataSetName] || [];
 
-                if (xScale !== 'time') {
-                    dataSets[dataSetName].push({ x: datum[xIndex], y: datum[yIndex] });
-                } else {
-                    console.log(datum[xIndex].toString());
-                    console.info(new Date(datum[xIndex]).getTimezoneOffset());
-                    dataSets[dataSetName].push({ x: new Date(datum[xIndex]), y: datum[yIndex] });
-                }
 
-
+                dataSets[dataSetName].push({ x: datum[xIndex], y: datum[yIndex] });
                 if (dataSets[dataSetName].length > config.maxLength) {
                     hasMaxLength = true;
                     dataSets[dataSetName].shift();
@@ -331,6 +325,245 @@ export default class BasicCharts extends React.Component {
     }
 
 
+    /**
+     * Generate visualization of the chart.
+     * @param props Current props of the component
+     * @param state Current state of the component
+     * @returns {Array} Array of chart elements to be displayed
+     */
+    generateVisualizations(props, state) {
+        const { config } = props;
+        const { height, width, chartArray, dataSets, xScale, ignoreArray } = state;
+        let chartComponents = [];
+        const legendItems = [];
+        let horizontal = false;
+        const lineCharts = [];
+        let areaCharts = [];
+        let barcharts = [];
+
+        chartArray.map((chart, chartIndex) => {
+            let addChart = false;
+            switch (chart.type) {
+                case 'line':
+                    Object.keys(chart.dataSetNames).map((dataSetName) => {
+                        legendItems.push({
+                            name: dataSetName,
+                            symbol: { fill: chart.dataSetNames[dataSetName] },
+                            chartIndex,
+                        });
+
+                        addChart = ignoreArray
+                            .filter(d => (d.name === dataSetName)).length > 0;
+
+                        console.log(dataSets[dataSetName].map(d => d));
+
+                        if (!addChart) {
+                            lineCharts.push(
+                                <VictoryGroup
+                                    key={`chart-${chart.id}-${chart.type}-${dataSetName}`}
+                                    data={dataSets[dataSetName]}
+                                    color={chart.dataSetNames[dataSetName]}
+                                >
+                                    <VictoryLine
+                                        style={{ data: { strokeWidth: config.charts[chartIndex].strokeWidth || null } }}
+                                    />
+                                    <VictoryPortal>
+                                        <VictoryScatter
+                                            labels={
+                                                d => `${config.x}:${Number(d.x).toFixed(2)}\n
+                                                ${config.charts[chartIndex].y}:${Number(d.y).toFixed(2)}`
+                                            }
+                                            labelComponent={
+                                                <VictoryTooltip
+                                                    orientation='bottom'
+                                                />
+                                            }
+                                            size={(d, a) => {
+                                                return a ? 20 : (config.charts[chartIndex].markRadius || 4);
+                                            }}
+                                            events={[{
+                                                target: 'data',
+                                                eventHandlers: {
+                                                    onClick: () => {
+                                                        return [
+                                                            {
+                                                                target: 'data',
+                                                                mutation: this._handleMouseEvent,
+                                                            },
+                                                        ];
+                                                    },
+                                                },
+                                            }]}
+
+                                        />
+                                    </VictoryPortal>
+                                </VictoryGroup>
+                            );
+                        }
+
+                        // alert(ReactDOMServer.renderToString(lineCharts[0]));
+
+
+                        return null;
+                    });
+                    break;
+                case 'area': {
+                    const areaLocal = [];
+
+                    Object.keys(chart.dataSetNames).map((dataSetName) => {
+                        legendItems.push({
+                            name: dataSetName,
+                            symbol: { fill: chart.dataSetNames[dataSetName] },
+                            chartIndex,
+                        });
+
+                        addChart = ignoreArray
+                            .filter(d => (d.name === dataSetName)).length > 0;
+
+                        if (!addChart) {
+                            areaLocal.push(
+                                <VictoryGroup
+                                    key={`chart-${chart.id}-${chart.type}-${dataSetName}`}
+                                    data={dataSets[dataSetName]}
+                                    color={chart.dataSetNames[dataSetName]}
+                                    style={{ data: { fillOpacity: config.charts[chartIndex].fillOpacity || 0.5 } }}
+                                >
+                                    <VictoryArea />
+                                    <VictoryPortal>
+                                        <VictoryScatter
+                                            labels={d => `${config.x}:${Number(d.x).toFixed(2)}\n
+                                                          ${config.charts[chartIndex].y}:${Number(d.y).toFixed(2)}`}
+                                            labelComponent={
+                                                <VictoryTooltip
+                                                    orientation='bottom'
+                                                />
+                                            }
+                                            size={(d, a) => {
+                                                return a ? 20 : config.charts[chartIndex].markRadius || 4;
+                                            }}
+                                            events={[{
+                                                target: 'data',
+                                                eventHandlers: {
+                                                    onClick: () => {
+                                                        return [
+                                                            {
+                                                                target: 'data',
+                                                                mutation: this._handleMouseEvent,
+                                                            },
+                                                        ];
+                                                    },
+                                                },
+                                            }]}
+                                        />
+                                    </VictoryPortal>
+                                </VictoryGroup>
+                            );
+                        }
+
+
+                        return null;
+                    });
+
+                    if (chart.mode === 'stacked') {
+                        areaCharts.push(
+                            <VictoryStack>
+                                {areaLocal}
+                            </VictoryStack>
+                        );
+                    } else {
+                        areaCharts = areaCharts.concat(areaLocal);
+                    }
+
+                    break;
+                }
+                case 'bar': {
+                    const localBar = [];
+
+                    horizontal = horizontal || chart.orientation === 'left';
+
+                    Object.keys(chart.dataSetNames).map((dataSetName) => {
+                        legendItems.push({
+                            name: dataSetName,
+                            symbol: { fill: chart.dataSetNames[dataSetName] },
+                            chartIndex,
+                        });
+                        addChart = ignoreArray
+                            .filter(d => (d.name === dataSetName)).length > 0;
+                        if (!addChart) {
+                            localBar.push(
+                                <VictoryBar
+                                    labels={d => `${config.x}:${d.x}\n${config.charts[chartIndex].y}:${d.y}`}
+                                    labelComponent={
+                                        <VictoryTooltip
+                                            orientation='bottom'
+                                        />
+                                    }
+                                    data={dataSets[dataSetName]}
+                                    color={chart.dataSetNames[dataSetName]}
+                                    events={[{
+                                        target: 'data',
+                                        eventHandlers: {
+                                            onClick: () => {
+                                                return [
+                                                    {
+                                                        target: 'data',
+                                                        mutation: this._handleMouseEvent,
+                                                    },
+                                                ];
+                                            },
+                                        },
+                                    }]}
+                                />
+                            );
+                        }
+
+
+                        return null;
+                    });
+
+                    if (chart.mode === 'stacked') {
+                        barcharts.push(
+                            <VictoryStack>
+                                {localBar}
+                            </VictoryStack>
+                        );
+                    } else {
+                        barcharts = barcharts.concat(localBar);
+                    }
+
+
+                    break;
+                }
+                default:
+                    console.error('Error in rendering unknown chart type');
+            }
+
+
+            return null;
+        });
+
+
+        if (areaCharts.length > 0) chartComponents = chartComponents.concat(areaCharts);
+        if (lineCharts.length > 0) chartComponents = chartComponents.concat(lineCharts);
+        if (barcharts.length > 0) {
+            const barWidth =
+                ((horizontal ?
+                    height : width) / (config.maxLength * (barcharts.length > 1 ? barcharts.length : 2))) - 3;
+
+            chartComponents.push(
+                <VictoryGroup
+                    horizontal={horizontal}
+                    offset={barWidth}
+                    style={{ data: { width: barWidth } }}
+                >
+                    {barcharts}
+                </VictoryGroup>
+            );
+        }
+
+        return chartComponents;
+    }
+
     render() {
         // this.handleAndSortData(this.props);
 
@@ -401,10 +634,9 @@ export default class BasicCharts extends React.Component {
                                     </VictoryPortal>
                                 </VictoryGroup>
                             );
-
                         }
 
-                        alert(ReactDOMServer.renderToString(lineCharts[0]));
+                        // alert(ReactDOMServer.renderToString(lineCharts[0]));
 
 
                         return null;
