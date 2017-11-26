2c45b0899c7256a5a103fd01c005934d4d1d557d
thusithakalugamage <thusitha32@yahoo.com>
2017-11-26 14:54:19 +0530

    custom theme and appbar added

--- samples/index.js
+++ samples/index.js
@@ -35,20 +35,56 @@ import LightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import DarkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+    grey700, grey100, grey300, grey400, grey500, darkBlack, fullBlack, deepOrange500,
+} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import './charts/css/default.css';
+
+const muiTheme = getMuiTheme({
+    fontFamily: 'Roboto, sans-serif',
+    palette: {
+        primary1Color: grey700,
+        primary2Color: grey500,
+        primary3Color: grey400,
+        accent1Color: deepOrange500,
+        accent2Color: grey100,
+        accent3Color: grey500,
+        textColor: darkBlack,
+        alternateTextColor: grey400,
+        canvasColor: grey100,
+        borderColor: grey300,
+        disabledColor: fade(darkBlack, 0.3),
+        pickerHeaderColor: grey700,
+        clockCircleColor: fade(darkBlack, 0.07),
+        shadowColor: fullBlack,
+    },
+});
 
 ReactDOM.render(
     <Router>
-        <MuiThemeProvider muiTheme={getMuiTheme(LightBaseTheme)}>
-        <div>
-            <Route exact path="/" component={App} />
-            <Route exact path="/line-charts" component={LineChart} />
-            <Route exact path="/bar-charts" component={BarChart} />
-            <Route exact path="/area-charts/" component={AreaChart} />
-            <Route exact path='/scatter-charts' component={ScatterPlot} />
-            <Route exact path='/pie-charts' component={PieChart} />
-            <Route exact path='/map-charts' component={MapChartConfigSample} />
-            <Route exact path='/number-charts' component={NumberChartConfigSample} />
-            <Route exact path='/table-charts' component={TableCharts} />
-        </div>
+        <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
+            <AppBar
+                title="react-vizgrammer"
+                iconStyleRight={{ color: '#ffffff',}}
+                iconElementRight={<IconButton
+                    iconClassName="muidocs-icon-custom-github" tooltip="bottom-center"
+                    tooltipPosition="top-center" href="https://github.com/wso2/react-vizgrammar"
+
+                />}/>
+            <div className={'lightBaseTheme'}>
+                <Route exact path="/" component={App} />
+                <Route exact path="/line-charts" component={LineChart} />
+                <Route exact path="/bar-charts" component={BarChart} />
+                <Route exact path="/area-charts/" component={AreaChart} />
+                <Route exact path='/scatter-charts' component={ScatterPlot} />
+                <Route exact path='/pie-charts' component={PieChart} />
+                <Route exact path='/map-charts' component={MapChartConfigSample} />
+                <Route exact path='/number-charts' component={NumberChartConfigSample} />
+                <Route exact path='/table-charts' component={TableCharts} />
+            </div>
         </MuiThemeProvider>
-    </Router>, document.getElementById('samples'));
+    </Router>,
+document.getElementById('samples'));
