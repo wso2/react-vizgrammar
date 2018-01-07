d7d9f6213cb1366d0e4ce5dc02c4e653bd9bcc01
Charuka Karunanayake <CharukaK@users.noreply.github.com>
2018-01-07 22:06:48 +0530

    added App page for the new samples

--- newSamples/index.jsx
+++ newSamples/index.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui';
+import { HashRouter as Router, Route} from 'react-router-dom';
+import App from './App';
 
-ReactDOM.render(<div />, document.getElementById('samples'));
+const theme = createMuiTheme({
+    palette: {
+        type: 'light',
+    },
+});
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme} >
+        <Router>
+            <Route exact path={'/'} component={App} />
+        </Router>
+    </MuiThemeProvider>, document.getElementById('samples'));
